95ca0fa4cd8578b05b393237ab2f04e6db73e9e3
sanderjn1 <112398478+sanderjn1@users.noreply.github.com>
2023-02-20 14:35:41 +0100

    lagt til flere funksjoner

--- conv/conv.go
+++ conv/conv.go
@@ -1,21 +1,35 @@
 package conv
 
+//Fra Fahrenheit til Celsius
 func FarhenheitToCelsius(value float64) float64 {
 
 	return (value - 32) * (5.0 / 9.0)
 }
 
+//Fra Kelvin til Celsius
 func KelvinToCelsius(value float64) float64 {
 
 	return (value - 273.15)
 }
 
+//Fra Celcius til Fahrenheit
 func CelcsiusToFahrenheit(value float64) float64 {
 
 	return (value*(9/5) + 32)
 }
 
+//Fra Celcsius til Kelvin
 func CelcsiusToKelvin(value float64) float64 {
 
 	return (value + 273.15)
+}
+
+//Fra Kelvin til Fahrenheit
+func KelvinToFahrenheit(value float64) float64 {
+	return (value-273.15)*9/5 + 32
+}
+
+//Fra Fahrenheit til Kelvin
+func FahrenheitToKelvin(value float64) float64 {
+	return (value-32)*5/9 + 273.15
 }
